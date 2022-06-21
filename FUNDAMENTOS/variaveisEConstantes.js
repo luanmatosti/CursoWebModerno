0264c82d7872c69bbcef83a733516ed9178a5e1d
luanmatosti <luanmatos.ti@gmail.com>
2022-06-21 15:20:03 -0300

    Subindo todos arquivos até o momento

---
+++ FUNDAMENTOS/variaveisEConstantes.js
@@ -0,0 +1,19 @@
+/*var a = 5;
+let b = 60;
+
+var a = 21; 
+b = 121;
+//obs. VAR permite declarar mais de uma vez uma variável
+console.log(a, b);*/
+
+const escola = "Escola Pinheiro";
+
+console.log(escola.charAt(8));
+console.log(escola.charCodeAt(8));
+console.log(escola.indexOf("c"));
+console.log(escola.substring(0,3))
+console.log(escola.substring(1))
+console.log("Nome: ".concat(escola).concat(" !"))
+console.log("Escola " + escola + "!");
+console.log(escola.replace('o', '0'));
+console.log("Leo, Natel, Beniel".split(";"))
