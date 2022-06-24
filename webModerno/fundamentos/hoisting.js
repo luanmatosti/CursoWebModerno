a8400d55fcd81e0ac7cba5c1629fee5ce78bc53c
luanmatosti <luanmatos.ti@gmail.com>
2022-06-24 16:05:47 -0300

    hosting

---
+++ webModerno/fundamentos/hoisting.js
@@ -0,0 +1,13 @@
+console.log('a =', a)
+var a = 2
+console.log('a =', a)
+
+console.log('b =', b)
+let b = 2 //hosting não ocorre com let
+console.log('b =', b)
+
+/*var a
+console.log('a =', a)
+a = 2
+console.log('a =', a)*/ //basicamente ele joga pra cima a variável, com var
+
